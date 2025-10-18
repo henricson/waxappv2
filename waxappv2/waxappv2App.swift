706ac309b65d08050540c91f27d9982451bc0275
Herman Henriksen <her@mans.no>
2025-10-18 19:44:43 +0200

    Initial Commit

---
+++ waxappv2/waxappv2App.swift
@@ -0,0 +1,17 @@
+//
+//  waxappv2App.swift
+//  waxappv2
+//
+//  Created by Herman Henriksen on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct waxappv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ waxappv2Tests/waxappv2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  waxappv2Tests.swift
+//  waxappv2Tests
+//
+//  Created by Herman Henriksen on 18/10/2025.
+//
+
+import Testing
+
+struct waxappv2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
